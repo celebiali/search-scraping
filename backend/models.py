971b46970937411113fd0f637a152fcc1528d627
Ali Rıza Çelebi <mailadresiniz@example.com>
2026-05-05 13:15:02 +0300

    Fix SQLAlchemy import error in models.py

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_all, Column, Integer, String, Float, DateTime, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
